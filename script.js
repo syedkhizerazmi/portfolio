4a45d2dd959f097f18b5b1f9e7d8022761d2ba4b
Syedkhizer <38943055+syedkhizerazmi@users.noreply.github.com>
2018-09-22 16:41:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 100) {
-	    $("nav").css("background" , "#12771c");
-	  }
-
-	  else{
-		  $("nav").css("background" , "transparent");  	
-	  }
-  })
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+            $("nav").css("background", "#12771c");
+        } else {
+            $("nav").css("background", "transparent");
+        }
+    })
 })
-function sidebar(){
 
-	document.querySelector(".hidden-span").style.display = "flex";
-	document.querySelector(".hidden-span").style.display = "flex";
+function sidebar() {
+
+    document.querySelector(".hidden-span").style.display = "flex";
+    document.querySelector(".hidden-span").style.display = "flex";
+}
+
+function fun() {
+
+    document.querySelector(".html").innerHTML = `
+		          <div class="html-c">Html</div>
+	`;
+
+    document.querySelector(".html-c").style.width = `60%`;
+    document.querySelector(".html-c").style["background-color"] = `green`;
+    document.querySelector(".html-c").style.transation = `2s`;
+
+
+
+
 }
